e08724e5ea31cf0624dc425cbccb7558dd4191a6
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-08-02 11:02:38 +0400

    Board Service with create + getAll + byId + update + Delete the Board

--- src/main/java/com/Codeline/Project/Management/Board/Services/BoardService.java
+++ src/main/java/com/Codeline/Project/Management/Board/Services/BoardService.java
@@ -1,5 +1,6 @@
-package com.example.projectmanagement.service;
+package com.Codeline.Project.Management.Board.Services;
 
+import com.Codeline.Project.Management.Board.Models.Board;
 import com.Codeline.Project.Management.Board.Repositories.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,4 +16,39 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    public Board createBoard(Board board) {
+        return boardRepository.save(board);
+    }
+
+    public List<Board> getAllBoards() {
+        return boardRepository.findAll();
+    }
+
+    public Board getBoardById(Long boardId) {
+        return boardRepository.findById(boardId).orElse(null);
+    }
+
+    public Board updateBoard(Long boardId, Board updatedBoard) {
+        Board existingBoard = boardRepository.findById(boardId).orElse(null);
+        if (existingBoard == null) {
+            return null; // Board not found
+        }
+
+        // Update the fields of the existing board with the values from the updated board
+        existingBoard.setTitle(updatedBoard.getTitle());
+        existingBoard.setColumns(updatedBoard.getColumns());
+
+        return boardRepository.save(existingBoard);
+    }
+
+    public boolean deleteBoard(Long boardId) {
+        Board board = boardRepository.findById(boardId).orElse(null);
+        if (board == null) {
+            return false; // Board not found
+        }
+        boardRepository.delete(board);
+        return true;
+    }
+
 }
+
