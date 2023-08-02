53ec4038d9889728202b7f13622a8c956a51385d
AliAlSubhi98 <alialsubhi1998@gmail.com>
2023-08-02 11:01:56 +0400

    Card Service with create + getAll + byId + update + Delete card from the Board

--- src/main/java/com/Codeline/Project/Management/Board/Services/CardService.java
+++ src/main/java/com/Codeline/Project/Management/Board/Services/CardService.java
@@ -1,11 +1,11 @@
 package com.Codeline.Project.Management.Board.Services;
 
+import com.Codeline.Project.Management.Board.Models.Card;
 import com.Codeline.Project.Management.Board.Repositories.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 @Service
 public class CardService {
     private final CardRepository cardRepository;
@@ -15,4 +15,39 @@ public class CardService {
         this.cardRepository = cardRepository;
     }
 
+    public Card createCard(Card card) {
+        return cardRepository.save(card);
+    }
+
+    public List<Card> getAllCardsFromBoard(Long boardId) {
+        return cardRepository.findByBoardId(boardId);
+    }
+
+    public Card getCardFromBoardById(Long boardId, Long cardId) {
+        return cardRepository.findByBoardIdAndId(boardId, cardId);
+    }
+
+    public Card updateCard(Card updatedCard) {
+        Card existingCard = cardRepository.findById(updatedCard.getId()).orElse(null);
+        if (existingCard == null) {
+            return null; // Card not found
+        }
+
+        existingCard.setTitle(updatedCard.getTitle());
+        existingCard.setDescription(updatedCard.getDescription());
+        existingCard.setSection(updatedCard.getSection());
+
+        return cardRepository.save(existingCard);
+    }
+
+    public boolean deleteCardFromBoard(Long boardId, Long cardId) {
+        Card card = cardRepository.findByBoardIdAndId(boardId, cardId);
+        if (card == null) {
+            return false; // Card not found
+        }
+        cardRepository.delete(card);
+        return true;
+    }
+
+    // Other methods as needed
 }
